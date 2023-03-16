dacfc192e0f2d57ea41e7ba76af2dc84d9de7078
asiasAtWork <asiasatwork@gmail.com>
2023-03-15 22:43:31 -0500

    Image update, countdown, and rsvp page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
+// Curved Header Text
 new CircleType(document.getElementById('curvedHeaderNames'))
     .radius(800);
 
 new CircleType(document.getElementById('curvedHeaderMarried'))
     .dir(-1)
     .radius(800);
+
+// Countdown Timer
+let weddingDate = new Date("Jun 29, 2023 18:30:00").getTime();
+
+let countdownTimer =  setInterval(function() {
+    // Retrieve today's date and time
+    let now = new Date().getTime();
+
+    // Distance between now and wedding date
+    let distance = weddingDate - now;
+
+    // Time calculations
+    let dayCount = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hourCount = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minuteCount = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let secondCount = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display countdown result
+    document.getElementById("days").innerHTML = dayCount;
+    document.getElementById("hours").innerHTML = hourCount;
+    document.getElementById("minutes").innerHTML = minuteCount;
+    document.getElementById("seconds").innerHTML = secondCount;
+
+    // On day of write text date confirmation text
+    
+    
+    // If countdown is up, remove counter and display updated text
+})
